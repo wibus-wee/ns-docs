86634af3933b03403f08bbb714423c15d54b21ea
wibus-wee <1596355173@qq.com>
2022-02-09 10:09:24 +0800

    fix route error

--- src/.vuepress/configs/sidebar/zh.ts
+++ src/.vuepress/configs/sidebar/zh.ts
@@ -3,13 +3,13 @@
  * @author: Wibus
  * @Date: 2022-02-09 08:55:38
  * @LastEditors: Wibus
- * @LastEditTime: 2022-02-09 09:07:28
+ * @LastEditTime: 2022-02-09 10:08:48
  * Coding With IU
  */
 import { SidebarConfig4Multiple } from "vuepress/config";
 import { getGuideSidebar, getDevelopmentSidebar } from "./shared";
 export const Sidebar4ZH: SidebarConfig4Multiple = {
   // NEED TO CHANGE ROUTE!!
-  '/guide/': getGuideSidebar('指南'),
-  '/development/': getDevelopmentSidebar('开发'),
+  '/zh/guide/': getGuideSidebar('指南'),
+  '/zh/development/': getDevelopmentSidebar('开发'),
 }
